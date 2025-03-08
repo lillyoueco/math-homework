b973646e00de4c89d8b0c8b4b963194528bfc0af
lillyoueco <lillyoueco@gmx.com>
2025-03-08 05:52:32 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,14 @@
+// Homework Problem 1: Find the area of a circle
+const calculateCircleArea = (radius: number): number => {
+  return Math.PI * radius ** 2;
+};
+
+// Homework Problem 2: Find the volume of a cube
+const calculateCubeVolume = (sideLength: number): number => {
+  return sideLength ** 3;
+};
+
+// Homework Problem 3: Find the perimeter of a rectangle
+const calculateRectanglePerimeter = (width: number, height: number): number => {
+  return 2 * (width + height);
+};
